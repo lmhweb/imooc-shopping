e05d0fd920dac3ebbc3bc0d20dd37f105432a72b
patrick <patrick.liu@evipt.com>
2018-01-20 17:01:37 +0800

    Default

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import GoodsList from '@/views/GoodsList'
+import Title from '@/views/Title';
+import Image from '@/views/Image';
 Vue.use(Router)
 
 export default new Router({
+  mode:"history",//history
+  //mode:"hash",//# hash
   routes: [
+    // {
+    //   //path: '/goods/:goodsId',动态路由 满足 /goods/:goodsId
+    //   path: '/goods/:goodsId/user/:name',//动态路由 同时满足两个格式
+    //   name: 'GoodsList',
+    //   component: GoodsList
+    // },
+    //嵌套路由
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/goods',
+      name: 'GoodsList',
+      component: GoodsList,
+      children:[
+        {
+          path:"title",
+          name: 'title',
+          component: Title,
+        },
+        {
+          path:"img",
+          name: 'img',
+          component: Image,
+        }
+      ]
     }
   ]
 })
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+       这是商品列表页面
+      <span>{{$route.params.goodsId}}</span><br>
+      <span>{{$route.params.name}}</span><br>
+      <div>
+        <router-link to="/goods/title">显示商品标题</router-link>
+        <router-link to="/goods/img">显示商品图片</router-link>
+      </div>
+      <div>
+      <router-view>
+
+      </router-view>
+      </div>
+    </div>
+</template>
+<style>
+
+</style>
+
+<script>
+    export default {
+        data(){
+            return {
+                msg: "hello vue"
+            }
+        }
+    }
+</script>
+
